e3190eb31886b2bff5b1799860482e67c52415ea
Jasper Reef <mail@jasperreef.com>
2026-07-05 12:46:44 +0200

    parallelize tests

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ import json
 import sys
 import os
 import difflib
+import concurrent.futures
+from pathlib import Path
 
 sys.path.insert(0, os.path.abspath('./bootstrap'))
 from bootstrap import __main__
@@ -32,8 +34,9 @@ if not os.path.isfile("test.wat"):
     if subprocess.run("bash ./run.sh ./native/main.watim compile ./test.watim -q > test.wat", shell=True).returncode != 0:
         exit(1)
 
+test_wat_path = os.path.abspath("test.wat")
 def parse_test_file(path: str) -> dict | None:
-    output = subprocess.run(f"wasmtime --dir=. -- test.wat read {path}", shell=True, stdout=subprocess.PIPE)
+    output = subprocess.run(f"wasmtime --dir=. -- {test_wat_path} read {path}", shell=True, stdout=subprocess.PIPE)
     if output.returncode != 0:
         return None
     return json.loads(output.stdout)
@@ -41,7 +44,7 @@ def parse_test_file(path: str) -> dict | None:
 def write_test_file(path: str, test: dict):
     output = subprocess.run(["wasmtime", "--dir=.", "--", "./test.wat", "write", path], input=bytes(json.dumps(test), 'UTF-8'))
     if output.returncode != 0:
-        print(output, file=sys.stderr)
+        print(output)
         return exit(1)
 
 @dataclass
@@ -60,19 +63,24 @@ if native:
             exit(1)
     watim_bin_path = os.path.realpath("./watim.wat")
 
-def run_native_compiler(args: List[str] | None, stdin: str):
+def run_native_compiler(cwd: str, args: List[str] | None, stdin: str):
     assert(watim_bin_path is not None)
-    compiler = subprocess.run(["wasmtime", "--dir=.", "--", watim_bin_path] + (args or ["compile", "-", "--quiet"]), input=bytes(stdin, 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    compiler = subprocess.run(
+            ["wasmtime", "--dir=.", "--", watim_bin_path] + (args or ["compile", "-", "--quiet"]),
+            input=bytes(stdin, 'UTF-8'),
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            cwd=cwd)
     return CompilerOutput(compiler.returncode, compiler.stdout.decode("UTF-8").strip(), compiler.stderr.decode("UTF-8").strip())
 
 bootstrap_entry_path = os.path.realpath("./bootstrap")
-def run_bootstrap_compiler(args: List[str] | None, stdin: str) -> CompilerOutput:
+def run_bootstrap_compiler(cwd, args: List[str] | None, stdin: str) -> CompilerOutput:
     main = bootstrap_compiler.main
     stderr = ""
     stdout = ""
     status = 0
     try:
-        stdout = main(["bootstrap.py"] + (args or ["compile", "-", "--quiet"]), stdin=stdin)
+        stdout = main(["bootstrap.py"] + (args or ["compile", "-", "--quiet"]), stdin=stdin, cwd=cwd)
     except bootstrap_compiler.CliArgException as e:
         stderr = e.message
         status = 1
@@ -90,21 +98,20 @@ def run_bootstrap_compiler(args: List[str] | None, stdin: str) -> CompilerOutput
 def accept(test_path: str):
     test = parse_test_file(test_path)
     if test is None:
-        print(f"{path}: failed to parse test file", file=sys.stderr)
+        print(f"{path}: failed to parse test file")
         return
-    os.chdir("./tests/fixtures")
     if native:
-        compiler = run_native_compiler(test['compiler-args'], test['compiler-stdin'])
+        compiler = run_native_compiler("./tests/fixtures", test['compiler-args'], test['compiler-stdin'])
     else:
-        compiler = run_bootstrap_compiler(test['compiler-args'], test['compiler-stdin'])
-    os.chdir("../..")
+        compiler = run_bootstrap_compiler("./tests/fixtures", test['compiler-args'], test['compiler-stdin'])
     stdout = None
     stderr = None
     status = None
     if compiler.returncode == 0 and test['status'] is not None:
-        with open('./out.wat', 'wb') as outwat:
+        watpath = f"/tmp/{os.getpid()}.wat"
+        with open(watpath, 'wb') as outwat:
             outwat.write(compiler.stdout.encode("UTF-8"))
-        program = subprocess.run(["wasmtime", "./out.wat"], input=bytes(test["stdin"] or "", 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        program = subprocess.run(["wasmtime", watpath], input=bytes(test["stdin"] or "", 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout = None if test['stdout'] is None else program.stdout.decode()
         stderr = None if test['stderr'] is None else program.stderr.decode()
         status = None if test['status'] is None else program.returncode
@@ -134,7 +141,7 @@ else:
 
 def print_mismatch(expected: str, actual: str):
     for line in difflib.unified_diff(expected.splitlines(), actual.splitlines(), fromfile='expected', tofile='actual'):
-        print(line, file=sys.stderr)
+        print(line)
 
 @dataclass
 class Test:
@@ -147,23 +154,19 @@ class Test:
     stdout: str | None
     stderr: str | None
 
-failed = False
-for path in tests:
+def run_test(path: str) -> bool:
     test = parse_test_file(path)
-    previous_failed = failed
-    failed = True
     if test is None:
-        print(f"{path}: failed to parse test file", file=sys.stderr)
-        exit(1)
+        print(f"{path}: failed to parse test file")
+        return False
     if test["compiler-stdin"] is None:
-        print(f"{path}: compiler-stdin ist missing", file=sys.stderr)
-        continue
-    os.chdir("./tests/fixtures")
+        print(f"{path}: compiler-stdin ist missing")
+        return False
+
     if native:
-        compiler = run_native_compiler(test['compiler-args'], test['compiler-stdin'])
+        compiler = run_native_compiler("./tests/fixtures", test['compiler-args'], test['compiler-stdin'])
     else:
-        compiler = run_bootstrap_compiler(test['compiler-args'], test['compiler-stdin'])
-    os.chdir("../..")
+        compiler = run_bootstrap_compiler("./tests/fixtures", test['compiler-args'], test['compiler-stdin'])
 
     def on_error():
         if interactive:
@@ -172,52 +175,59 @@ for path in tests:
                 accept(path)
 
     if test['compiler-status'] is not None and compiler.returncode != test['compiler-status']:
-        print(f"{path}: expected different compiler status:", file=sys.stderr)
-        print(f"Expected:\n{test['compiler-status']}", file=sys.stderr)
-        print(f"Actual:\n{compiler.returncode}", file=sys.stderr)
-        print(f"compiler-stderr was: {compiler.stderr}", file=sys.stderr)
+        print(f"{path}: expected different compiler status:")
+        print(f"Expected:\n{test['compiler-status']}")
+        print(f"Actual:\n{compiler.returncode}")
+        print(f"compiler-stderr was: {compiler.stderr}")
         on_error()
-        continue
+        return False
     if compare_stderr and test['compiler-stderr'] is not None and compiler.stderr != test['compiler-stderr'].strip():
-        print(f"{path}: expected different compiler stderr:", file=sys.stderr)
+        print(f"{path}: expected different compiler stderr:")
         print_mismatch(test['compiler-stderr'], compiler.stderr)
         on_error()
-        continue
+        return False
     if test['compiler-stdout'] is not None and compiler.stdout != test['compiler-stdout'].strip():
-        print(f"{path}: expected different compiler stdout:", file=sys.stderr)
+        print(f"{path}: expected different compiler stdout:")
         print_mismatch(test['compiler-stdout'], compiler.stdout)
-        print(f"stderr was: {compiler.stderr}", file=sys.stderr)
+        print(f"stderr was: {compiler.stderr}")
         on_error()
-        continue
+        return False
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        outwat_path = os.path.join(tmpdir, 'out.wat')
-        with open(outwat_path, 'wb') as outwat:
-            outwat.write(compiler.stdout.encode("UTF-8"))
-        if compiler.returncode == 0 and test['status'] is not None:
-            program = subprocess.run(["wasmtime", outwat_path], input=bytes(test["stdin"] or "", 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if test['stderr'] is not None and program.stderr.strip() != test['stderr'].encode('UTF-8').strip():
-                print(f"{path}: expected different stderr:", file=sys.stderr)
-                print(f"Expected:\n{test['stderr']}", file=sys.stderr)
-                print(f"Actual:\n{program.stderr.decode('UTF-8')}", file=sys.stderr)
-                on_error()
-                continue
-            if test['stdout'] is not None and program.stdout.strip() != test['stdout'].encode('UTF-8').strip():
-                print(f"{path}: expected different stdout:", file=sys.stderr)
-                print(f"Expected:\n{test['stdout']}", file=sys.stderr)
-                print(f"Actual:\n{program.stdout.decode('UTF-8')}", file=sys.stderr)
-                on_error()
-                continue
-            if test['status'] is not None and program.returncode != test['status']:
-                print(f"{path}: expected different status:", file=sys.stderr)
-                print(f"Expected:\n{test['status']}", file=sys.stderr)
-                print(f"Actual:\n{program.returncode}", file=sys.stderr)
-                if test['stderr'] is None:
-                    print(f"stderr was: {program.stderr.decode('UTF-8')}", file=sys.stderr)
-                on_error()
-                continue
-    failed = previous_failed
+    outwat_path = Path(os.path.join(tmpdir, os.path.basename(path))).with_suffix('.wat')
+    with open(outwat_path, 'wb') as outwat:
+        outwat.write(compiler.stdout.encode("UTF-8"))
+    if compiler.returncode == 0 and test['status'] is not None:
+        program = subprocess.run(["wasmtime", outwat_path], input=bytes(test["stdin"] or "", 'UTF-8'), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if test['stderr'] is not None and program.stderr.strip() != test['stderr'].encode('UTF-8').strip():
+            print(f"{path}: expected different stderr:")
+            print(f"Expected:\n{test['stderr']}")
+            print(f"Actual:\n{program.stderr.decode('UTF-8')}")
+            on_error()
+            return False
+        if test['stdout'] is not None and program.stdout.strip() != test['stdout'].encode('UTF-8').strip():
+            print(f"{path}: expected different stdout:")
+            print(f"Expected:\n{test['stdout']}")
+            print(f"Actual:\n{program.stdout.decode('UTF-8')}")
+            on_error()
+            return False
+        if test['status'] is not None and program.returncode != test['status']:
+            print(f"{path}: expected different status:")
+            print(f"Expected:\n{test['status']}")
+            print(f"Actual:\n{program.returncode}")
+            if test['stderr'] is None:
+                print(f"stderr was: {program.stderr.decode('UTF-8')}")
+            on_error()
+            return False
     print(f"{path} passed")
+    return True
+
+failed = False
+with tempfile.TemporaryDirectory() as tmpdir:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures: List[concurrent.futures.Future[bool]] = []
+        for path in tests:
+            futures.append(executor.submit(run_test, path))
+        failed = not all(future.result() for future in futures)
 
 if failed:
     exit(1)
